43ed0cf3c06eadb6c832970ac379fdb01ec3d5c5
Sebastian Adam Prokuski <sprokuski@gmail.com>
2016-09-08 15:51:58 -0700

    implement private set_post method in post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,14 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+
   def index
     @posts = Post.all
   end
+
   def new
     @post = Post.new
   end
+
   def create
     @post = Post.new(post_params)
     if @post.save
@@ -13,22 +17,22 @@ class PostsController < ApplicationController
         render 'new'
     end
   end
+
   def show
-    @post = Post.find(params[:id])
   end
+
   def edit
-    @post = Post.find(params[:id])
   end
+
   def update
-    @post = Post.find(params[:id])
     if @post.update(post_params)
      redirect_to post_path(@post)
     else
      render :edit
     end
   end
+
   def destroy
-    @post = Post.find(params[:id])
     @post.destroy
     redirect_to posts_path
   end
@@ -37,5 +41,9 @@ class PostsController < ApplicationController
 
   def post_params
     params.require(:post).permit(:image, :caption)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 end
