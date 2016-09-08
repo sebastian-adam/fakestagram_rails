490a8f1f24cd6b66359436bee72b3a8f7af07aec
Sebastian Adam Prokuski <sprokuski@gmail.com>
2016-09-08 15:33:05 -0700

    implement ability to edit posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -16,6 +16,17 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+  def edit
+    @post = Post.find(params[:id])
+  end
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+     redirect_to post_path(@post)
+    else
+     render :edit
+    end
+  end
 
   private
 
